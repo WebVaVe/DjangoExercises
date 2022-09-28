3fca0cca47bf8dcfac60d08e34df31bed3a6ce12
ARIYANEM <emami8637@gmail.com>
2022-09-28 04:41:24 +0330

    debug and add name app

--- taskEx/urls.py
+++ taskEx/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 import datetime
 from . import views
 
+app_name = "taskEx"
 urlpatterns = [
     path ("", views.index,name = "taskEx"),
     path ("add", views.add, name = "add")
